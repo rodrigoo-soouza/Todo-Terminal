dac2e95b1fbb3efb51104754b688701c3102df23
rodrigoo-soouza <digosouzafonseca@gmail.com>
2024-01-19 23:38:38 -0300

    Commit Inicio

---
+++ Models/Todo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyTodoApp.Models;
+
+public class Todo
+{
+    public int Id { get; set; }
+    
+    [DisplayName("Título")]
+    [Required(ErrorMessage = "Campo obrigatório")]
+    public string Title { get; set; } = String.Empty;
+    
+    [DisplayName("Concluído")]
+    public bool Done { get; set; }
+    
+    [DisplayName("Criado em")]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    
+    [DisplayName("Última atualização")]
+    public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+    public string User { get; set; } = String.Empty;
+}
